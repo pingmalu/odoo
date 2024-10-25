85071e4fc68b9999724cd050f8474377085285b8
malu <malu@malu.me>
2024-10-25 16:07:59 +0800

    autopush

--- xaddons/estate/__manifest__.py
+++ xaddons/estate/__manifest__.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 {
-    'name': 'estate',
-    'version': '0.1',
-    'category': 'Sales/estate',
-    'sequence': 15,
-    'summary': 'Track leads and close opportunities',
-    'website': 'https://malu.me',
-    'depends': [
-        'base',
+    "name": "estate",
+    "version": "0.1",
+    "category": "Sales/estate",
+    "sequence": 15,
+    "summary": "Track leads and close opportunities",
+    "website": "https://malu.me",
+    "license": "LGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "depends": [
+        "base",
     ],
 }
