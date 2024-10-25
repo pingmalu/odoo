640e5b11cd7ad350d5c3d8f259adfe6269cdb7ed
malu <malu@malu.me>
2024-10-25 15:01:18 +0800

    autopush

---
+++ xaddons/estate/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'estate',
+    'version': '0.1',
+    'category': 'Sales/estate',
+    'sequence': 15,
+    'summary': 'Track leads and close opportunities',
+    'website': 'https://malu.me',
+    'depends': [
+        'base',
+    ],
+}
---
+++ xaddons/estate/models/estate_property.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = "estate.property"
+    _description = "Property"
+
+    name = fields.Char(string="名称")
+    description = fields.Text(string="描述")
+    postcode = fields.Char(string="邮编")
+    date_availability = fields.Date(string="可售日期")
+    expected_price = fields.Float(string="期望价格")
+    selling_price = fields.Float(string="销售价格")
+    bedrooms = fields.Integer(string="卧室数量")
+    living_area = fields.Integer(string="使用面积")
+    facades = fields.Integer(string="面")
+    garage = fields.Boolean(string="是否有车库")
+    garden = fields.Boolean(string="是否有花园")
+    garden_area = fields.Integer(string="花园面积")
+    garden_orientation = fields.Selection(
+        string="花园方向",
+        selection=[("north", "北"), ("south", "南"), ("east", "东"), ("west", "西")],
+    )
